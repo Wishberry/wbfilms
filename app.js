09ff012c571dd88957c825c758dade7a6e674494
Ankur Verma <ankurv3rma@gmail.com>
2018-07-10 02:13:40 +0530

    ssl

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sslRedirect = require('heroku-ssl-redirect');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +14,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(sslRedirect(['production'], 301));
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'src', 'images', 'favicon.png')));
