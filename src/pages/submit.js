62507b74613e636860124b0e0619477118bf492d
Ankur Verma <ankurv3rma@gmail.com>
2018-07-09 20:39:45 +0530

    form multiple past work fields

--- src/pages/submit.js
+++ src/pages/submit.js
@@ -14,12 +14,13 @@ import Button from '../components/button';
 const submit = "https://s3.ap-south-1.amazonaws.com/aw-files-1-118130771025/submit-cover.jpg";
 
 
-class SubmitPage extends React.PureComponent {
+class SubmitPage extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      wordCount: 0
+      wordCount: 0,
+      pastWork: []
     };
   }
 
@@ -55,9 +56,15 @@ class SubmitPage extends React.PureComponent {
     this.setState({wordCount: wordArray.length});
   }
   onSubmit = (values) => {
-    const data = values;
-    data.phoneNumber = data.areaCode.concat(" ", data.phoneNumber);
-    delete data.areaCode;
+    const data = {...values};
+    if(data.pastWork) {
+      data.pastWork = data.pastWork.join(", ");
+    }
+    let phoneNumber = data.phoneNumber.join("");
+    if(phoneNumber.charAt(0) === '+'){
+      phoneNumber = phoneNumber.slice(1);
+    }
+    data.phoneNumber = parseInt(phoneNumber);
     event.preventDefault();
     axios.post('https://wishberry-films.herokuapp.com/api/creator/lead/create', data)
     .then(function (response) {
@@ -107,6 +114,7 @@ class SubmitPage extends React.PureComponent {
             <div className="form-container">
 
               <Formik
+                initialValues={{ pastWork: [''] }}
                 onSubmit={this.onSubmit}
                 render={({
                   values,
@@ -131,10 +139,10 @@ class SubmitPage extends React.PureComponent {
                         <label>
                           Phone Number
                         </label>
-                        <FieldArray>
+                        <FieldArray name="phoneNumber">
                           <div className="form-input-group">
-                            <Field className="form-input tel-short" maxLength={4} type="tel" name="areaCode" placeholder="+91" required/>
-                            <Field className="form-input tel-long" maxLength={10} type="tel" name="phoneNumber" placeholder="Your Phone Number" required/>
+                            <Field className="form-input tel-short" maxLength={4} type="tel" name="phoneNumber.0" placeholder="+91" required/>
+                            <Field className="form-input tel-long" maxLength={10} type="tel" name="phoneNumber.1" placeholder="Your Phone Number" required/>
                           </div>
                         </FieldArray>
                       </div>
@@ -195,10 +203,59 @@ class SubmitPage extends React.PureComponent {
                         <div className="help-text">*We mainly work with films that are under 2 Crores.</div>
                       </div>
                       <div className="form-component">
-                        <label>
-                          Past Work
-                        </label>
-                        <Field className="form-input" placeholder="Add links, example - www.abc.com" type="text" name="pastWork"/>
+                          <label>
+                            Past Work (Upto 5)
+                          </label>
+                          <FieldArray
+                            name="pastWork"
+                            render={arrayHelpers => (
+                              <div className="form-input-list">
+                                {values.pastWork && values.pastWork.map( (item, index) =>
+                                  <div key={index} className="input-with-action">
+                                    <Field className="form-input" placeholder="Add links, example - www.abc.com" type="text" name={`pastWork.${index}`}/>
+                                    {index === 0 ?
+                                      <a
+                                        className={values.pastWork.length >= 5 ? "add-input disabled" : "add-input"}
+                                        onClick={() => arrayHelpers.push('')}
+                                      >
+                                        &#43;
+                                      </a>
+                                      :
+                                      <a
+                                        className="remove-input"
+                                        onClick={() => arrayHelpers.remove(index)}
+                                      >
+                                        -
+                                      </a>
+                                    }
+                                  </div>
+                                )}
+                              </div>
+                            )}
+                          >
+                            {/* <div className="form-input-list">
+                              <div className="input-with-action">
+                                <Field className="form-input" placeholder="Add links, example - www.abc.com" type="text" name="pastWork.0"/>
+                                <a
+                                  className="add-input"
+                                  onClick={this.addField}
+                                >
+                                  &#43;
+                                </a>
+                              </div>
+                              {this.state.pastWork.map( (item, index) =>
+                                <div key={index} className="input-with-action">
+                                  <Field className="form-input" placeholder="Add links, example - www.abc.com" type="text" name={`pastWork.${item}`}/>
+                                  <a
+                                    className="remove-input"
+                                    onClick={() => this.removeField(index)}
+                                  >
+                                    -
+                                  </a>
+                                </div>
+                              )}
+                            </div> */}
+                          </FieldArray>
                       </div>
 
                     </div>
