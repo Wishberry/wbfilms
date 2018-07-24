5e03a4d4f5017e3b5f9ab5f8ff2f7448ee9f8387
Ankur Verma <ankurv3rma@gmail.com>
2018-07-24 15:11:13 +0530

    fixed dependency version

--- public/path---film-zollywood-a0e39f21c11f6a62c5ab.js
+++ public/path---film-zollywood-a0e39f21c11f6a62c5ab.js
@@ -1,2 +1,2 @@
-webpackJsonp([54902129941149],{494:function(t,n){t.exports={pathContext:{}}}});
+webpackJsonp([54902129941149],{493:function(t,n){t.exports={pathContext:{}}}});
 //# sourceMappingURL=path---film-zollywood-a0e39f21c11f6a62c5ab.js.map
